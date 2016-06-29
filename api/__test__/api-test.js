d1a2cec7cefb23a159b2242ab22ed9c8e2ce3586
Sky Chen <skychen@yahoo-inc.com>
2016-06-29 01:02:03 -0700

    add tests

---
+++ api/__test__/api-test.js
@@ -0,0 +1,15 @@
+import {expect} from 'chai';
+
+describe('dummy', () => {
+
+  describe('a test', () => {
+
+    it('is passed', () => {
+      let state = 42;
+
+      expect(state).to.equal(42);
+    });
+
+  });
+
+});
